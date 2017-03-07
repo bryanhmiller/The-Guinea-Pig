0e631b2962b2b3798a9081ea58cb4e062b6fdf3c
bryanhmiller <bryankalos@hotmail.com>
2017-03-06 20:31:23 -0600

    Added listener that completed first objective in exercise

--- events.js
+++ events.js
@@ -1,5 +1,11 @@
 var whichSectionGotClicked = document.getElementsByClassName("article-section");
 var targetDiv = document.getElementById("output-target");
-
 console.log(whichSectionGotClicked);
 
+function myFunction(event){
+	console.log(event.target.innerHTML);
+	targetDiv.innerHTML = "<h2>" + "You clicked on the " + event.target.innerHTML + " section." + "</h2>"
+}
+
+
+document.body.addEventListener("click", myFunction);
