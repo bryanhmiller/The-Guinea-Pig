559ffc20a4da75b0e2a2d9140d60eb9d05824daf
bryanhmiller <bryankalos@hotmail.com>
2017-03-06 23:45:32 -0600

    Added event listener to mouse events in numbers 2 and 3

--- events.js
+++ events.js
@@ -1,11 +1,24 @@
-var whichSectionGotClicked = document.getElementsByClassName("article-section");
 var targetDiv = document.getElementById("output-target");
-console.log(whichSectionGotClicked);
+var pageTitleMouseEvent= document.getElementById("page-title");
 
-function myFunction(event){
-	console.log(event.target.innerHTML);
-	targetDiv.innerHTML = "<h2>" + "You clicked on the " + event.target.innerHTML + " section." + "</h2>"
+
+// 1. When any section is clicked the output target text should be "You clicked on 
+// the {text of the section} section" 
+function clickedSectionIdentifier(event){
+	console.log(event.target);
+	if (event.target.className === "article-section") {
+		targetDiv.innerHTML = "<h2>" + "You clicked on the " + event.target.innerHTML + " section." + "</h2>"
+	}	
 }
 
+// 2. When the mouse is over the h1 tag, the output element should contain the text 
+// "You moved your mouse over the header".
+// 3. When the mouse leaves the h1 tag, the output element should contain the text 
+// "You left me!!".	
+function pageTitleAction() {
+	console.log(event.target);
+}	
 
-document.body.addEventListener("click", myFunction);
+pageTitleMouseEvent.addEventListener("mouseenter", pageTitleAction);
+pageTitleMouseEvent.addEventListener("mouseleave", pageTitleAction);
+document.body.addEventListener("click", clickedSectionIdentifier);
