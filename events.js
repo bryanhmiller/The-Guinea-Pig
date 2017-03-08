7ac0e81dbea35710689a2ec86cc02a28f8c7e380
bryanhmiller <bryankalos@hotmail.com>
2017-03-07 20:02:30 -0600

    Added mouseenter and mouseleave events

--- events.js
+++ events.js
@@ -7,18 +7,27 @@ var pageTitleMouseEvent= document.getElementById("page-title");
 function clickedSectionIdentifier(event){
 	console.log(event.target);
 	if (event.target.className === "article-section") {
-		targetDiv.innerHTML = "<h2>" + "You clicked on the " + event.target.innerHTML + " section." + "</h2>"
+		targetDiv.innerHTML = "<h2>" + "You clicked on the " + event.target.innerHTML + " section." + "</h2>";
 	}	
 }
 
 // 2. When the mouse is over the h1 tag, the output element should contain the text 
 // "You moved your mouse over the header".
-// 3. When the mouse leaves the h1 tag, the output element should contain the text 
-// "You left me!!".	
-function pageTitleAction() {
+function pageTitleActionEnter() {
 	console.log(event.target);
+	targetDiv.innerHTML = "<h2>You moved your mouse over the header</h2>";
 }	
 
-pageTitleMouseEvent.addEventListener("mouseenter", pageTitleAction);
-pageTitleMouseEvent.addEventListener("mouseleave", pageTitleAction);
+// 3. When the mouse leaves the h1 tag, the output element should contain the text 
+// "You left me!!".	
+function pageTitleActionLeave() {
+	console.log(event.target);
+	targetDiv.innerHTML = "<h2>You left me!!</h2>";
+}	
+
+// 4. When you type characters into the input field, the output element should mirror the 
+// text in the input field.
+
+pageTitleMouseEvent.addEventListener("mouseenter", pageTitleActionEnter);
+pageTitleMouseEvent.addEventListener("mouseleave", pageTitleActionLeave);
 document.body.addEventListener("click", clickedSectionIdentifier);
