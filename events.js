e39bda7bf3783444cce5a2277b9774323776f1ef
bryanhmiller <bryankalos@hotmail.com>
2017-03-11 10:46:32 -0600

    Corrected function to change color, was adding an undefined to innerHTML, but didn't need innerHTML. Code is now working properly

--- events.js
+++ events.js
@@ -6,7 +6,6 @@ var addColorButton = document.getElementById("add-color");
 // 1. When any section is clicked the output target text should be "You clicked on 
 // the {text of the section} section" 
 function clickedSectionIdentifier(event){
-	console.log(event.target);
 	if (event.target.className === "article-section") {
 		targetDiv.innerHTML = "<h2>" + "You clicked on the " + event.target.innerHTML + " section." + "</h2>";
 	}	
@@ -15,14 +14,12 @@ function clickedSectionIdentifier(event){
 // 2. When the mouse is over the h1 tag, the output element should contain the text 
 // "You moved your mouse over the header".
 function pageTitleActionEnter() {
-	console.log(event.target);
 	targetDiv.innerHTML = "<h2>You moved your mouse over the header</h2>";
 }	
 
 // 3. When the mouse leaves the h1 tag, the output element should contain the text 
 // "You left me!!".	
 function pageTitleActionLeave() {
-	console.log(event.target);
 	targetDiv.innerHTML = "<h2>You left me!!</h2>";
 }	
 
@@ -36,8 +33,15 @@ function typedInput() {
 // 5. When you click the "Add color" button, the guinea-pig element's text color should 
 // change to blue.
 function colorChange() {
-	targetDiv.innerHTML += targetDiv.classList.add("color-me-blue");
+	console.log(event.target);
+	targetDiv.classList.add("color-me-blue");
 }
+
+// 6. When you click the "Hulkify" button, the guinea-pig element's font size should become 
+// much larger.
+
+
+
 
 document.body.addEventListener("click", clickedSectionIdentifier);
 pageTitleMouseEvent.addEventListener("mouseenter", pageTitleActionEnter);
